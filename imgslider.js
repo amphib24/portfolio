8a123ee77bdf1788f4adf4965cb5782be8b5690d
JEFF BLANCHARD <BLANCHARDJD19@COMCAST.NET>
2017-01-07 20:30:13 -0800

    shit
    this shit wont fucntion right!

--- imgslider.js
+++ imgslider.js
@@ -1,26 +1,27 @@
 $(function imgClick() {
-  var ul = $('.placeholder');
-  var slide_count = ul.children().length;
+  var ul = $('.placeholder ul');
+  var slide_count = ul.contents('li').length;
   var slide_width_pc = 100.0 / slide_count;
   var slide_index = 0;
 
-   ul.find('li').each(function(indx){
-     var left_percent = (slide_width_pc * indx) + '%';
-     $(this).css({'left': left_percent});
-     $(this)
+  ul.find('li').each(function(indx){
+    var left_percent = (slide_width_pc * indx) + '%';
+    $(this).css({'left': left_percent});
+    $(this)
 .css({width:(100 / slide_count) + '%'});
   });
-  $(' button.previous').click(function() {
-    console.log('prev button clicked');
+
+  $('.previous').click(function() {
     slide(slide_index - 1);
   });
-  $('button.next').click(function() {
-    console.log('next button clicked');
+
+  $('.next').click(function() {
     slide(slide_index + 1);
   });
+
   function slide(new_slide_index) {
     if (new_slide_index < 0 || new_slide_index >= slide_count) return;
     var margin_left_pc = (new_slide_index * (-100)) + '%';
-    ul.animate({'margin-left': margin_left_pc},400,imgClick())
+    ul.animate({'margin-left': margin_left_pc},4000,imgClick())
     slide_index = new_slide_index}
 });
